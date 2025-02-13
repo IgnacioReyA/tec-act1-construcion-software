32b3b10d2c872177aa6a4553b6e2bc76ce9baf43
IgnacioReyA <ignacio.reyarslangul@gmail.com>
2025-02-13 09:30:46 -0600

    Tarea final

--- src/app/components/Rectangle.tsx
+++ src/app/components/Rectangle.tsx
@@ -1,16 +1,7 @@
-import classNames from 'classnames';
-
 interface RectangleProps {
-    color: 'green' | 'blue';
+    color: string;
 }
 
 export default function Rectangle({ color }: RectangleProps) {
-    const colors: { [key in RectangleProps['color']]: string } = {
-        green: 'bg-green-500',
-        blue: 'bg-blue-500',
-    };
-
-    const rectangleClass = classNames('w-64 h-32', colors[color]);
-
-    return <div className={rectangleClass}></div>;
+    return <div style={{ backgroundColor: color }}  className="w-64 h-32"></div>;
 }
